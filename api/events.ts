465c8ef0deda83269a2e51547f0ad0d45e59c467
Carmit Vaknin <carmit.vk@gmail.com>
2026-09-23 20:22:03 +0300

    Use default Vercel API runtime

--- api/events.ts
+++ api/events.ts
@@ -1,8 +1,6 @@
 import * as XLSX from 'xlsx'
 import { readSession } from './auth'
 
-export const config = { runtime: 'nodejs' }
-
 type VercelRequest = { method?: string; body?: unknown; headers?: Record<string, string | undefined> }
 type VercelResponse = { status: (code: number) => VercelResponse; json: (body: unknown) => void }
 
