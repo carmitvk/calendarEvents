a65d2b19dd8f0181d79fceb6c6bb93cb62f8a78f
Carmit Vaknin <carmit.vk@gmail.com>
2026-09-23 12:42:09 +0300

    Rename list view button

--- src/App.tsx
+++ src/App.tsx
@@ -597,7 +597,7 @@ function App() {
           <span className="excel-actions">
             <input ref={fileInputRef} className="visually-hidden" type="file" accept=".xlsm,.xlsx" onChange={loadWorkbook} />
             <button type="button" className="excel-button" onClick={() => setIsDownloadDialogOpen(true)}>הורד נתונים כ Excel</button>
-            <button type="button" className="excel-button" onClick={() => setIsExcelView((current) => !current)}>{isExcelView ? 'הצג כיומן' : 'הצג כאקסל'}</button>
+            <button type="button" className="excel-button" onClick={() => setIsExcelView((current) => !current)}>{isExcelView ? 'הצג כיומן' : 'הצג כרשימה'}</button>
           </span>
         </div>
 
